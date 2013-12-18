c7612c1ad9bc3a023e6a3fe625a44a859d79a3aa
hanish92 <hanishsingla@intersoftprofessional.com>
2013-12-18 13:30:17 +0530

    step2

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+
+// HTTP
+define('HTTP_SERVER', 'http://plantbulb.co.uk/demo/');
+define('HTTP_IMAGE', 'http://plantbulb.co.uk/demo/image/');
+define('HTTP_ADMIN', 'http://plantbulb.co.uk/demo/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://plantbulb.co.uk/demo/');
+define('HTTPS_IMAGE', 'http://plantbulb.co.uk/demo/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencartsites/plantbulb.co.uk/demo/catalog/');
+define('DIR_SYSTEM', '/var/www/html/opencartsites/plantbulb.co.uk/demo/system/');
+define('DIR_DATABASE', '/var/www/html/opencartsites/plantbulb.co.uk/demo/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/opencartsites/plantbulb.co.uk/demo/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencartsites/plantbulb.co.uk/demo/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/opencartsites/plantbulb.co.uk/demo/system/config/');
+define('DIR_IMAGE', '/var/www/html/opencartsites/plantbulb.co.uk/demo/image/');
+define('DIR_CACHE', '/var/www/html/opencartsites/plantbulb.co.uk/demo/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencartsites/plantbulb.co.uk/demo/download/');
+//define('DIR_IMAGE_BACKBASE', 'C:\inetpub\wwwroot\backbase.fullserviceplatform.com\boom\attachments/');
+define('DIR_LOGS', '/var/www/html/opencartsites/plantbulb.co.uk/demo/system/logs/');
+
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'mydb.ck6w2al7sgpk.eu-west-1.rds.amazonaws.com');
+define('DB_USERNAME', 'tarsem');
+define('DB_PASSWORD', 'tarsem121#');
+define('DB_DATABASE', 'demo_opencart');
+define('DB_PREFIX', '');
+
+
+?>
